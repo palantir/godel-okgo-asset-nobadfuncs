b878c4c43cc37fdb58d65bbeac29be0db3330735
Nick Miyake <nmiyake@palantir.com>
2019-12-06 16:19:05 -0800

    Update format settings
    ==NO_CHANGELOG==

--- nobadfuncs/config/internal/legacy/config.go
+++ nobadfuncs/config/internal/legacy/config.go
@@ -17,11 +17,10 @@ package legacy
 import (
 	"encoding/json"
 
+	v0 "github.com/palantir/godel-okgo-asset-nobadfuncs/nobadfuncs/config/internal/v0"
 	"github.com/palantir/godel/pkg/versionedconfig"
 	"github.com/pkg/errors"
 	yaml "gopkg.in/yaml.v2"
-
-	v0 "github.com/palantir/godel-okgo-asset-nobadfuncs/nobadfuncs/config/internal/v0"
 )
 
 type Config struct {
